576ee5c4ff8cc3446837116e7bc32bbc6a46f6ad
provideforme <evan.a.bernard@gmail.com>
2022-03-02 21:27:56 -0500

    Color Change

--- models/flight.js
+++ models/flight.js
@@ -35,7 +35,10 @@ const flightSchema = new Schema ({
   },
 
   departs: {
-    type: Date,
+    type: Date, 
+    default: function() {
+    return new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+    },
   },
 
   tickets: [ticketSchema],
